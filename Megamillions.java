74d4a5253b0c24e147f083bba84fcd58a0bbbfe6
davidpolanco27 <54686584+davidpolanco27@users.noreply.github.com>
2019-09-03 16:47:07 -0400

    Add files via upload

---
+++ Megamillions.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ * @author David
+ * description:
+ * 
+ *
+ */
+
+	//import the scanner class
+public class Megamillions {
+	public static void main(String [] args) {
+}}
+		Random randGen= new Random ();
+		int rnum1= randGen.nextInt(60)+1
+		system.out.println(randGen.nextInt(60));
+	//Declare variable to hold amount of dollar
+	//Declare variable for cost of ticket
+	//
+	//prompt the user for the amount
+	// of dollars to bet on
+	// store amount into amount of dollar
+	// variable
+	
+	//prompt the user to enter six numbers
+	//create a varible for each value
+	
+	//Randomly generate six numbers and compare
